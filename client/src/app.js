98646df08ea7e958fcf9e1e12cfba55366346b02
sandip <sandip.mistry283@gmail.com>
2025-03-25 14:02:41 +0000

    Structure

--- client/src/app.js
+++ client/src/app.js
@@ -1,65 +1,61 @@
-
 document
   .getElementById("movieForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let movieName = document.getElementById("movieInput").value;
+    let movieName = document.getElementById("suggestions").value;
     let responseMessage = document.getElementById("responseMessage");
 
     if (movieName.trim() !== "") {
       responseMessage.textContent = `Thank you for recommending "${movieName}". Tune in for further developments!`;
-      document.getElementById("movieInput").value = ""; // Clear input after submission
+      document.getElementById("suggestions").value = ""; // Clear input after submission
     }
   });
 
-const allMovies = document.getElementById("allMovies")
+const allMovies = document.getElementById("allMovies");
 async function fetchMovies() {
-    const response = await fetch("http://localhost:8080/allmovies");
-    const allMoviesData = await response.json();
+  const response = await fetch("http://localhost:8080/allmovies");
+  const allMoviesData = await response.json();
 }
 fetchMovies();
 
+const moviesGenres = ["Superhero", "Sci-Fi", "Comedy", "Action"];
 
-const moviesGenres = [ 
-  "Superhero",
-  "Sci-Fi",
-  "Comedy",
-  "Action",] 
+async function getMoviesByGenre(genre) {
+  const response = await fetch(
+    `http://localhost:8080/moviesbygenre?genre=${genre}`
+  );
 
-async function getMoviesByGenre (genre) {
-  const response = await fetch(`http://localhost:8080/moviesbygenre?genre=${genre}`);
-  
-  const data= await response.json();
-  return data; 
-
+  const data = await response.json();
+  return data;
 }
 
 const allMoviesContainer = document.getElementById("allMoviesContainer");
 
-
-function displayMovies () {
+function displayMovies() {
   moviesGenres.forEach(async function (genre) {
     const movies = await getMoviesByGenre(genre);
     const genreContainer = document.createElement("section");
     const individualMovieImg = document.createElement("img");
     const currentGenreH2 = document.createElement("h2");
     const shuffleButton = document.createElement("button");
-    shuffleButton.textContent="Shuffle";
+    shuffleButton.textContent = "Shuffle";
     shuffleButton.addEventListener("click", function () {
-      individualMovieImg.src = movies[Math.floor(Math.random()*movies.length)].image;
-      
-    })
-    genreContainer.appendChild(shuffleButton);
+      individualMovieImg.src =
+        movies[Math.floor(Math.random() * movies.length)].image;
+    });
+    genreContainer.appendChild(currentGenreH2);
     genreContainer.className = "genreContainer";
     currentGenreH2.textContent = genre;
-    genreContainer.appendChild(currentGenreH2);
+    genreContainer.appendChild(shuffleButton);
 
-    individualMovieImg.src = movies[Math.floor(Math.random()*movies.length)].image;
+    genreContainer.id = `section-${genre.toLowerCase()}`;
+
+    individualMovieImg.src =
+      movies[Math.floor(Math.random() * movies.length)].image;
     genreContainer.appendChild(individualMovieImg);
     allMoviesContainer.appendChild(genreContainer);
-    })
+  });
+}
 
-  }
-
-  displayMovies();
+displayMovies();
